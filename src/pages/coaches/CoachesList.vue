7b495b8b13f8e66c188f2f5698bd3b03ee11feda
James Muriithi <muriithijames556@gmail.com>
2021-03-29 23:48:17 +0300

    route components

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        Filter
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,17 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetails from "./pages/coaches/CoachDetails.vue";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachRegister from "./pages/coaches/CoachRegister.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import RequestsReceived from "./pages/requests/RequestsReceived.vue";
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {path:'/', redirect: '/coaches'},
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: '/contact', component: null }]
+      component: CoachDetails,
+      children: [{ path: '/contact', component: ContactCoach }]
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
+    { path: '/register', component: CoachRegister },
+    { path: '/requests', component: RequestsReceived },
     {path:'/:notFound(.*)', component: null},
   ]
 });
