9c6e3f2d877f8dae9314daa9ab9220a1b767656e
James Muriithi <muriithijames556@gmail.com>
2021-03-29 23:56:58 +0300

    the header

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -9,18 +9,3 @@ export default {
     
 }
 </script>
-<style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
-
-* {
-  box-sizing: border-box;
-}
-
-html {
-  font-family: "Roboto", sans-serif;
-}
-
-body {
-  margin: 0;
-}
-</style>
